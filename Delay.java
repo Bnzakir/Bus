44b8ab090462c560f26328640d921af01233cb7d
Homan Cheung <homan.cheung@student.manchester.ac.uk>
2015-05-04 15:55:13 +0100

    poopdsfsd

--- Delay.java
+++ Delay.java
@@ -1,3 +1,4 @@
+// Delay class for delays
 import java.util.*;
 import static java.util.Calendar.*;
 public class Delay {
@@ -6,19 +7,18 @@ public class Delay {
      * @param args the command line arguments
      */
 
+    // Get value of certain delay when given  sevice number and date
     // -1 if no entry, 0 if cancelled, else returns delay time
     public static int getDelay(int service_number, Date date)
     {
         return database.busDatabase.get_int_delay("delays", service_number, date, "delay");
     } 
 
-
+    // Add new delay record into table
     public static void addDelayToDatabase(int service_number, int bus_number, int delay, String message, Date date)
     {
         database.busDatabase.new_record("delays", new Object[][]{{"service_number", service_number}, {"bus_number", bus_number}, {"delay", delay}, {"message", message}, {"date", date}});
     }
-
-
 
 
 
